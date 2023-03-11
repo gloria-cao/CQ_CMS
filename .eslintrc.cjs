a45015337bac673a6d82cfee4451dd2112e28d59
caoqin <@.com>
2023-03-11 10:42:15 +0800

    initialize item

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting',
+    '@vue/prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    
+  }
+}
